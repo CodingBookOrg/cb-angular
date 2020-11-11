8d0d35fbdab206aa90e42683ce95c19b44cc1634
orhan <orhanu2020@gmail.com>
2020-11-11 15:17:23 -0500

    style binding

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -2,8 +2,12 @@
 import { Component } from '@angular/core';
 @Component({
     selector: "courses",
+    /** https://www.w3schools.com/Jsref/dom_obj_style.asp */
     template: `<h2>{{ title}}</h2>
-        <button class="btn btn-primary" [class.active]="isActive">Bootstrap</button>`
+        <button class="btn btn-primary"
+        [style.backgroundColor]="isActive ? 'blue' : 'red'">
+        Bootstrap
+        </button>`
 })
 export class CoursesComponent {
     title: string = "List of courses";
