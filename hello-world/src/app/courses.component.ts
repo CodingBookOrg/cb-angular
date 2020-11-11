180b66e7188884f019dd5dcf0b35411b5562ab37
orhan <orhanu2020@gmail.com>
2020-11-11 15:02:55 -0500

    Class binding

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 @Component({
     selector: "courses",
     template: `<h2>{{ title}}</h2>
-        <button class="btn btn-primary">Bootstrap</button>`
+        <button class="btn btn-primary" [class.active]="isActive">Bootstrap</button>`
 })
 export class CoursesComponent {
-    title: string = "List of courses"
-    
+    title: string = "List of courses";
+    isActive = false;
 }
 
