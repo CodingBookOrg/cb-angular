5058b6ae62c42e462e6e5b5e6b1e9393304c3fbb
orhan <orhanu2020@gmail.com>
2020-11-11 02:25:18 -0500

    HTML Tempaltes & String interpolation

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: "courses",
-    template: "<h2>Courses<h2>"
+    template: `<h2>{{ title + " (Count:" + getCount() + ")"}}</h2>
+                <app-course></app-course>`
 })
 export class CoursesComponent {
+    title: string = "List of courses"
 
+    getCount  = () : number => {
+        return 5;
+    }
 }
 
