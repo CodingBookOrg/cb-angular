6edae10fabe6746d9c16d3a855446e515771e24e
orhan <orhanu2020@gmail.com>
2020-11-11 02:46:33 -0500

    Simple Directive

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -3,10 +3,13 @@ import { Component } from '@angular/core';
 @Component({
     selector: "courses",
     template: `<h2>{{ title + " (Count:" + getCount() + ")"}}</h2>
-                <app-course></app-course>`
+                <ul>
+                    <li *ngFor="let course of courses">{{ course }}</li>
+                </ul>`
 })
 export class CoursesComponent {
     title: string = "List of courses"
+    courses : string[] = ["Java", "Python", "Clooud Computing"];
 
     getCount  = () : number => {
         return 5;
