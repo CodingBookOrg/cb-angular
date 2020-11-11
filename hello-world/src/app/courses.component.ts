d6a51a6eef98bc7c680fa952b5045c2821be8c52
orhan <orhanu2020@gmail.com>
2020-11-11 09:28:01 -0500

    Using attr.attributeName in case HTML attribute is not mapped to DOM element

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -1,14 +1,24 @@
+
 import { Component } from '@angular/core';
 import { CoursesService } from './courses.service';
 
 @Component({
     selector: "courses",
     template: `<h2>{{ title}}</h2>
-                <img [src]="organizationImageUrl">`
+                <table>
+                    <tr>
+                        <td [attr.colspan]="colSpan" 
+                        *ngFor="let course of courses">{{ course }}</td>
+                    </tr>
+                </table>`
 })
 export class CoursesComponent {
     title: string = "List of courses"
-    organizationImageUrl: string = 
-        "https://codingbook.org/wp-content/uploads/2020/08/cropped-logo2-2-1.png"
+    courses : string[];
+    colSpan = 5
+
+    constructor(service: CoursesService) {
+        this.courses = service.getCourses()
+    }
 }
 
--- hello-world/src/app/courses.service.ts
+++ hello-world/src/app/courses.service.ts
@@ -1,5 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: "root" })
 export class CoursesService {
     getCourses = () : string[] => {
-        return ["Java", "Python", "Clooud Computing"];
+        return ["Java", "Python", "Cloud Computing"];
     }
 }
