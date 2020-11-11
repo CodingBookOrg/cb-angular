6023ef7d0d60ce268325b567e94378eb7e44101a
orhan <orhanu2020@gmail.com>
2020-11-11 12:07:19 -0500

    Using Bootstrap

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -1,24 +1,12 @@
 
 import { Component } from '@angular/core';
-import { CoursesService } from './courses.service';
-
 @Component({
     selector: "courses",
     template: `<h2>{{ title}}</h2>
-                <table>
-                    <tr>
-                        <td [attr.colspan]="colSpan" 
-                        *ngFor="let course of courses">{{ course }}</td>
-                    </tr>
-                </table>`
+        <button class="btn btn-primary">Bootstrap</button>`
 })
 export class CoursesComponent {
     title: string = "List of courses"
-    courses : string[];
-    colSpan = 5
-
-    constructor(service: CoursesService) {
-        this.courses = service.getCourses()
-    }
+    
 }
 
