b2f324d8b7334ad3ba26b1c49453b839c422a6b9
orhan <orhanu2020@gmail.com>
2020-11-11 19:47:36 -0500

    Event Binding

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -4,13 +4,16 @@ import { Component } from '@angular/core';
     selector: "courses",
     /** https://www.w3schools.com/Jsref/dom_obj_style.asp */
     template: `<h2>{{ title}}</h2>
-        <button class="btn btn-primary"
-        [style.backgroundColor]="isActive ? 'blue' : 'red'">
+        <button class="btn btn-primary" (click)="onClick()">
         Bootstrap
         </button>`
 })
 export class CoursesComponent {
     title: string = "List of courses";
-    isActive = false;
+    isActive = true;
+
+    onClick() {
+        console.log("Button Clicked");
+    }
 }
 
