c9827dd4e44e9109ff328229245be4143ccafd50
orhan <orhanu2020@gmail.com>
2020-11-11 22:53:18 -0500

    Pipes : Built in pipes

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -2,16 +2,21 @@
 import { Component } from '@angular/core';
 @Component({
     selector: "courses",
-    template: `<h2>{{ title}}</h2>
-        <input [(ngModel)]="email" (keyup.enter)="onKeyup()"/>
+    template: `<br>
+    {{ course.title | uppercase | lowercase }}<br>
+    {{ course.rating | number:'1.2-2' }}<br>
+    {{ course.students | number }}<br>
+    {{ course.price | currency }}<br>
+    {{ course.releaseDate | date:'shortDate'}}
         `
 })
 export class CoursesComponent {
-    title: string = "List of courses";
-    email = "info@codingbook.org";
-
-    onKeyup() {
-        console.log(this.email);
+    course = {
+        title: "Java",
+        rating: 4.9785,
+        students: 30123,
+        price: 190.95,
+        releaseDate: new Date(2019, 3, 5)
     }
 }
 
