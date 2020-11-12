2de2ea83972cb46598f1a1c3288c2c445a7c113b
orhan <orhanu2020@gmail.com>
2020-11-12 07:28:29 -0500

    Custom pipe with parameter

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: "courses",
     template: `<br>
-        {{ title | summary }}`
+        {{ title | summary:30 }}`
 })
 export class CoursesComponent {
     title = `Java course teaches java development 
