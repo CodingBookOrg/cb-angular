4a18d118f570f7e1bdd967cca43dac17e0639616
orhan <orhanu2020@gmail.com>
2020-11-11 20:17:54 -0500

    Event Filter

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -4,16 +4,14 @@ import { Component } from '@angular/core';
     selector: "courses",
     /** https://www.w3schools.com/Jsref/dom_obj_style.asp */
     template: `<h2>{{ title}}</h2>
-        <button class="btn btn-primary" (click)="onClick()">
-        Bootstrap
-        </button>`
+        <input type="text" (keyup.enter)="onKeyup($event)">`
 })
 export class CoursesComponent {
     title: string = "List of courses";
     isActive = true;
 
-    onClick() {
-        console.log("Button Clicked");
+    onKeyup($event) {
+        console.log("Enter pressed");
     }
 }
 
