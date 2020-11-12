8cad42f8f691b2e41bff10052672120d72d30a0e
orhan <orhanu2020@gmail.com>
2020-11-11 20:24:17 -0500

    Template variables

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -4,14 +4,20 @@ import { Component } from '@angular/core';
     selector: "courses",
     /** https://www.w3schools.com/Jsref/dom_obj_style.asp */
     template: `<h2>{{ title}}</h2>
-        <input type="text" (keyup.enter)="onKeyup($event)">`
+        <input type="text" (keyup.enter)="onKeyup($event)">
+        <input #email type="text" (keyup.enter)="onKeyup2(email.value)">
+        `
 })
 export class CoursesComponent {
     title: string = "List of courses";
     isActive = true;
 
     onKeyup($event) {
-        console.log("Enter pressed");
+        console.log($event.target.value);
+    }
+
+    onKeyup2(email) {
+        console.log(email);
     }
 }
 
