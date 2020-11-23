99cb45aa1ab35d79668bd8c2761063ae07eb7f8f
orhan <orhanu2020@gmail.com>
2020-11-23 02:43:11 -0500

    ngSwitch

--- topics/src/app/app.component.ts
+++ topics/src/app/app.component.ts
@@ -1,14 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { IFavoriteChangedEventArgs } from "./favorite/Out/IFavoriteChangedEventArgs";
+import {Component,} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  ngOnInit(): void {
-    console.log(this.courses.length);
-  }
-  courses: string [] = ["Java", "Python"]
+export class AppComponent {
+  viewMode = "otherwise"
 }
