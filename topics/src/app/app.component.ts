b8bbafe27143f4373ef353694e95572230df747f
orhan <orhanu2020@gmail.com>
2020-11-23 18:35:11 -0500

    safe traversal operator

--- topics/src/app/app.component.ts
+++ topics/src/app/app.component.ts
@@ -6,7 +6,12 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  canSave: boolean = false;
+  task = {
+    title: "Task 1",
+    assignee: {
+      name: "John"
+    }
+  }
 }
 
 class Course {
